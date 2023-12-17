f8a8745d225aa4112c9610eeaa2b6b3eb9a0fd62
MazidulHasan <md.mazidulhasan1@gmail.com>
2023-12-17 18:06:38 +0600

    Updated with sentiant url

--- callApi.py
+++ callApi.py
@@ -1,7 +1,11 @@
 import requests
 
 def callApiWithZipName(zipName):
-    api_url = 'http://192.168.50.92:5000/process_data'
+    #api_url = 'http://192.168.50.92:5000/process_data'
+    #api_url = 'https://computervisioncvp.pythonanywhere.com/process_data'
+    #api_url = 'https://whale-app-5cpna.ondigitalocean.app/process_data'
+    api_url = 'https://sentiant.net/process_data'
+    
     file_name = zipName
 
     data = {'file_name': file_name}
